93be0eb0a192db988a6ef134c13508221db344cf
Jefferson-Fasso <jefferson.pastuna@est.ikiam.edu.ec>
2024-10-06 16:14:27 -0500

    Plot to Phytochemical Analysis journal.

---
+++ notame_Results_Plots.R
@@ -0,0 +1,31 @@
+##### Plots to journal #####
+
+# Join all figures
+Figure_1 <- arrangeGrob(figure_1a,
+                        figure_2a,
+                        figure_1b,
+                        figure_2b,
+                        figure_1c,
+                        figure_2c,
+                        layout_matrix = rbind(c(1, 2),
+                                              c(3, 4),
+                                              c(3, 4),
+                                              c(5, 6),
+                                              c(5,6)))
+# Adding letters
+figure_one <- ggpubr::as_ggplot(Figure_1) +
+  draw_plot_label(label = LETTERS[1:6],
+                  x = c(0, 0.5, 0, 0.5, 0, 0.5),
+                  y = c(.99, .99, .790, .790, .350, .350))
+# (*.pdf) file
+ggsave(filename = "PCA_Journal_Plot/Figure_1_PCA_Journal.pdf",
+       plot = figure_one, width = 150, height = 180,
+       units = "mm", dpi = 300, scale = 2.5)
+# (*.png) file
+ggsave(filename = "PCA_Journal_Plot/Figure_1_PCA_Journal.png",
+       plot = figure_one, width = 150, height = 180,
+       units = "mm", dpi = 300, scale = 2.5)
+# (*.jpg) file
+ggsave(filename = "PCA_Journal_Plot/Figure_1_PCA_Journal.jpg",
+       plot = figure_one, width = 150, height = 180,
+       units = "mm", dpi = 300, scale = 2.5)
